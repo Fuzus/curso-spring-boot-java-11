b44919fdac595fd34f758179d07b33e1a22ac8ef
Fuzus <matheusdamata2000@gmail.com>
2021-09-30 15:01:45 -0300

    Category

---
+++ src/main/java/com/educandoweb/curso/servicies/CategoryService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.curso.servicies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.curso.entities.enums.Category;
+import com.educandoweb.curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> Category = repository.findById(id);
+		return Category.get();
+	}
+}

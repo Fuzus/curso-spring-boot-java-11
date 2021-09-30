a2ec9efe8f9f3cd76fef842e5986d98ac7465c50
Fuzus <matheusdamata2000@gmail.com>
2021-09-30 15:20:53 -0300

    Product

--- src/main/java/com/educandoweb/curso/repositories/CategoryRepository.java
+++ src/main/java/com/educandoweb/curso/repositories/CategoryRepository.java
@@ -2,7 +2,7 @@ package com.educandoweb.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.educandoweb.curso.entities.enums.Category;
+import com.educandoweb.curso.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long>{
 	
--- src/main/java/com/educandoweb/curso/servicies/CategoryService.java
+++ src/main/java/com/educandoweb/curso/servicies/CategoryService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.educandoweb.curso.entities.enums.Category;
+import com.educandoweb.curso.entities.Category;
 import com.educandoweb.curso.repositories.CategoryRepository;
 
 @Service
